dadf2062a8717f945ef3f18f4dbed890e1d7dbe9
anitang0413 <anitang0413@gmail.com>
2020-02-15 00:30:30 +0800

    Create getEmpSatisfy.php

---
+++ data/chartData/getEmpSatisfy.php
@@ -0,0 +1,8 @@
+
+    <?php
+       $json = file_get_contents("https://apiservice.mol.gov.tw/OdService/rest/datastore/A17000000J-020078-urh");
+      // $obj = json_decode($json);
+
+      echo $json;
+     // echo json_encode($json,JSON_UNESCAPED_UNICODE)
+    ?>
